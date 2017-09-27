1595e3e000161bbca354ae79a94c293325121a92
Dylan <dylanangus@hotmail.com>
2017-09-27 08:37:59 +0100

    initital

---
+++ SupervisedLearningPractice/preprocessing_template.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 14 13:08:30 2017
+
+@author: dylan
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Part 1 Preprocessing
+
+#loading dataset
+dataset = pd.read_csv('housing.csv', delim_whitespace=True, header=None)
+X = dataset.iloc[:, 0:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+
+#Splitting data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
